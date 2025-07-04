ce8dd116516778211f1becb3adc9ef599cea0cde
riponmondalbd <riponmondal149@gmail.com>
2025-07-05 04:11:23 +0600

    make educations section

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -12,6 +12,7 @@ import {
 import { SiNextdotjs, SiTailwindcss } from "react-icons/si";
 
 import { getYearDifferenceFromDate } from "@/components/Home/Stats/YearCalculation/YearCalculation";
+import { EducationData } from "@/components/Resume/EducationData/EducationData";
 import { ExperienceData } from "@/components/Resume/ExperienceData/ExperienceData";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
@@ -158,9 +159,39 @@ export default function ResumePage() {
                 </ScrollArea>
               </div>
             </TabsContent>
+
             {/* education */}
             <TabsContent value="education" className="w-full">
-              education
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{EducationData.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {EducationData.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {EducationData.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <span className="text-amber-200">
+                            {item.duration}
+                          </span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                            {item.degree}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            {/* make dot */}
+                            <span className="w-[6px] h-[6px] rounded-full bg-amber-200"></span>
+                            <p className="text-white/60">{item.institution}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
             </TabsContent>
             {/* skills */}
             <TabsContent value="skills" className="w-full">
