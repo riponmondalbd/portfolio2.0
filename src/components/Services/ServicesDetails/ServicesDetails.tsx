92e57b68802a70909fa30564f34023780ca6410e
riponmondalbd <riponmondal149@gmail.com>
2025-07-05 01:50:28 +0600

    make ServicesDetails function

---
+++ src/components/Services/ServicesDetails/ServicesDetails.tsx
@@ -0,0 +1,26 @@
+export const ServicesDetails = [
+  {
+    num: "01",
+    title: "Web Development",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    href: "",
+  },
+  {
+    num: "02",
+    title: "UI/UX Designer",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    href: "",
+  },
+  {
+    num: "03",
+    title: "Logo Designer",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    href: "",
+  },
+  {
+    num: "04",
+    title: "SEO",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    href: "",
+  },
+];
