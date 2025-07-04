e53af766cc5e995e134256ebc51e3e9419cb2873
riponmondalbd <riponmondal149@gmail.com>
2025-07-05 03:14:06 +0600

    make dummy educational data

---
+++ src/components/Resume/EducationData/EducationData.ts
@@ -0,0 +1,43 @@
+export const EducationData = {
+  icon: "",
+  title: "My education",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem voluptatem modi vel rerum! Vitae autem non eius minus, explicabo labore.",
+  items: [
+    {
+      institution: "Online Course",
+      degree: "Full Stack Development",
+      duration: "2023",
+    },
+    {
+      institution: "CodeAcademy",
+      degree: "Front-End Developer",
+      duration: "2022",
+    },
+    {
+      institution: "Online Course",
+      degree: "Programming Courser",
+      duration: "2022",
+    },
+    {
+      institution: "Tech Institute",
+      degree: "Certified Web Developer",
+      duration: "2021",
+    },
+    {
+      institution: "CodeAcademy",
+      degree: "Front-End Developer",
+      duration: "2022",
+    },
+    {
+      institution: "Online Course",
+      degree: "Programming Courser",
+      duration: "2022",
+    },
+    {
+      institution: "Tech Institute",
+      degree: "Certified Web Developer",
+      duration: "2021",
+    },
+  ],
+};
