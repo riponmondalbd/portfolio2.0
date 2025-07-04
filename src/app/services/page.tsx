dee9ffa54a2eb6bf7a951205fbee99a94c75900a
riponmondalbd <riponmondal149@gmail.com>
2025-07-05 02:42:53 +0600

    complete service page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -24,15 +24,22 @@ export default function ServicesPage() {
             >
               {/* top bar */}
               <div className="w-full flex justify-between items-center">
-                <div className="text-5xl font-extrabold">{service.num}</div>
-                <Link href={service.href}>
-                  <BsArrowDownRight />
+                <div className="text-5xl font-extrabold text-outline transition-all duration-500 group-hover-text-outline-hover">
+                  {service.num}
+                </div>
+                <Link
+                  href={service.href}
+                  className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-[#00ff99] transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                >
+                  <BsArrowDownRight className="text-[#1c1b22] text-3xl" />
                 </Link>
               </div>
               {/* service title */}
-              <h2>{service.title}</h2>
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-[#00ff99] transition-all duration-500">
+                {service.title}
+              </h2>
               {/* service description */}
-              <p>{service.description}</p>
+              <p className="text-white/60">{service.description}</p>
               {/* border */}
               <div className="border-b border-white/20 w-full"></div>
             </div>
