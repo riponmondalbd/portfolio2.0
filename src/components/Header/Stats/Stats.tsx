69e001294a358d80a0d997a845ee4e21b4accc14
riponmondalbd <riponmondal149@gmail.com>
2025-07-05 01:22:00 +0600

    make counter with animation and finally complete the home page

--- src/components/Header/Stats/Stats.tsx
+++ src/components/Header/Stats/Stats.tsx
@@ -4,7 +4,7 @@ import CountUp from "react-countup";
 import { getYearDifferenceFromDate } from "./YearCalculation/YearCalculation";
 
 export default function Stats() {
-  const yearOfExperience = getYearDifferenceFromDate("2023-01-01");
+  const yearOfExperience = getYearDifferenceFromDate("2022-01-01");
 
   const stats = [
     {
@@ -16,26 +16,40 @@ export default function Stats() {
       text: "Projects completed",
     },
     {
-      num: 8,
+      num: 20,
       text: "Technologies mastered",
     },
     {
-      num: 500,
+      num: 3000,
       text: "Code commits",
     },
   ];
   return (
-    <div>
-      {stats.map((item, index) => (
-        <div key={index}>
-          <CountUp
-            end={item.num}
-            duration={5}
-            delay={2}
-            className="text-4xl xl:text-6xl font-extrabold"
-          />
+    <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
+      <div className="container mx-auto">
+        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
+          {stats.map((item, index) => (
+            <div
+              key={index}
+              className="flex-1 flex gap-4 items-center justify-center xl:justify-start"
+            >
+              <CountUp
+                end={item.num}
+                duration={5}
+                delay={2}
+                className="text-4xl xl:text-6xl font-extrabold"
+              />
+              <p
+                className={`${
+                  item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
+                } leading-snug text-white/80`}
+              >
+                {item.text}
+              </p>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </div>
+    </section>
   );
 }
