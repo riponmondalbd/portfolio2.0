9b3d20b974dc47b330517f12491684b49c498cad
riponmondalbd <riponmondal149@gmail.com>
2025-07-05 03:09:03 +0600

    create experience data

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,3 +1,48 @@
+"use client";
+
+import { getYearDifferenceFromDate } from "@/components/Home/Stats/YearCalculation/YearCalculation";
+
 export default function ResumePage() {
+  const yearOfExperience = getYearDifferenceFromDate("2022-01-01");
+
+  const about = {
+    title: "About me",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem voluptatem modi vel rerum! Vitae autem non eius minus, explicabo labore.",
+    info: [
+      {
+        fieldName: "Name",
+        fieldValue: "Ripon Mondal",
+      },
+      {
+        fieldName: "Phone",
+        fieldValue: "+880 1956 149980",
+      },
+      {
+        fieldName: "Experience",
+        fieldValue: `${yearOfExperience}+ Years`,
+      },
+      {
+        fieldName: "GitHub",
+        fieldValue: "riponmondalbd",
+      },
+      {
+        fieldName: "Nationality",
+        fieldValue: "Bangladeshi",
+      },
+      {
+        fieldName: "Email",
+        fieldValue: "contact.riponmondal@gmail.com",
+      },
+      {
+        fieldName: "Freelance",
+        fieldValue: "Available",
+      },
+      {
+        fieldName: "Language",
+        fieldValue: "English, Bengali",
+      },
+    ],
+  };
   return <div>resume page</div>;
 }
---
+++ src/components/Resume/ExperienceData/ExperienceData.ts
@@ -0,0 +1,41 @@
+// import { FaHtml5 } from "react-icons/fa";
+// import { SiTailwindcss } from "react-icons/si";
+
+export const ExperienceData = {
+  icon: "",
+  title: "My experience",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem voluptatem modi vel rerum! Vitae autem non eius minus, explicabo labore.",
+  items: [
+    {
+      company: "Tech Solutions Inc",
+      position: "Full Stack Developer",
+      duration: "2022 - Present",
+    },
+    {
+      company: "Web Design Studio",
+      position: "Front-End Developer Intern",
+      duration: "Summer 2021",
+    },
+    {
+      company: "E-commerce Startup",
+      position: "Freelancer Web Developer",
+      duration: "2020 - 2021",
+    },
+    {
+      company: "Tech Academy",
+      position: "Teaching Assistant",
+      duration: "2019 - 2020",
+    },
+    {
+      company: "Digital Agency",
+      position: "UI/UX Designer",
+      duration: "2018 - 2019",
+    },
+    {
+      company: "Software Developer Firm",
+      position: "Junior Developer",
+      duration: "2017 - 2018",
+    },
+  ],
+};
