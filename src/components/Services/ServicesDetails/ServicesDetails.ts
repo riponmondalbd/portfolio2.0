ff470bd061c8d16333bfdea02db129cb650f6849
riponmondalbd <riponmondal149@gmail.com>
2025-07-06 00:39:14 +0600

    update serviceDetails real rata

--- src/components/Services/ServicesDetails/ServicesDetails.ts
+++ src/components/Services/ServicesDetails/ServicesDetails.ts
@@ -1,26 +1,30 @@
 export const ServicesDetails = [
   {
     num: "01",
-    title: "Web Development",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    href: "",
+    title: "MERN Stack Development",
+    description:
+      "MERN Stack Development is a full-stack JavaScript framework comprising MongoDB, Express.js, React.js, and Node.js for building dynamic web applications.",
+    href: "/contact",
   },
   {
     num: "02",
-    title: "UI/UX Designer",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    href: "",
+    title: "Frontend Development",
+    description:
+      "Frontend development involves creating the user interface and experience of a website or application using technologies like HTML, CSS, Tailwind.css, JavaScript and React.",
+    href: "/contact",
   },
   {
     num: "03",
-    title: "Logo Designer",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    href: "",
+    title: "Backend Development",
+    description:
+      "Backend development involves creating and maintaining the server-side logic, databases, and APIs that power the functionality of web and mobile applications.",
+    href: "/contact",
   },
   {
     num: "04",
-    title: "SEO",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    href: "",
+    title: "Full Stack Development",
+    description:
+      "Full Stack Development with Next.js enables building scalable web applications using React on the frontend and powerful backend features like API routes and server-side rendering.",
+    href: "/contact",
   },
 ];
