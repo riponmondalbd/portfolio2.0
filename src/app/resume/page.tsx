35a41982a4fc539a6e6cdaebe36ade54972d8384
riponmondalbd <riponmondal149@gmail.com>
2025-07-06 02:41:59 +0600

    also contact page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -2,7 +2,7 @@ import ResumeContent from "@/components/Resume/ResumeContent/ResumeContent";
 
 export const metadata = {
   title: "Resume",
-  description: "Resume page here show my all projects",
+  description: "Resume page here show my resume",
 };
 
 export default function ResumePage() {
